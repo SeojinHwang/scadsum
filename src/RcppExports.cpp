b2660e80587a57028993876b6af94bacd200e2fc
SeojinHwang <id1544@korea.ac.kr>
2021-09-20 23:13:08 +0900

    handling divergence
    print warning if not converge instead of stopping

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,6 +9,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // countlines
 int countlines(const char* fileName);
 static SEXP _scadsum_countlines_try(SEXP fileNameSEXP) {
